c20996957a5b5f981047d19122d2dcb1b434bc55
yongkico <58337410+yongkico@users.noreply.github.com>
2020-07-14 20:24:22 +0700

    Update drop.js

--- js/drop.js
+++ js/drop.js
@@ -4,22 +4,26 @@
  *
  */
 function readFile(input) {
-    if (input.files) {
-        inputLength = input.files.length;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-        for (var i = 0; i < inputLength; i++) {
-            var reader = new FileReader()
-            reader.onload = function (e) {
-                var htmlPreview =
-                    '<img width="200" src="' + e.target.result + '" />' + '   ';
-                $('.box-body').append(htmlPreview);
+        reader.onload = function (e) {
+            var htmlPreview =
+                '<img width="200" src="' + e.target.result + '" />' +
+                '<p>' + input.files[0].name + '</p>';
+            var wrapperZone = $(input).parent();
+            var previewZone = $(input).parent().parent().find('.preview-zone');
+            var boxZone = $(input).parent().parent().find('.preview-zone').find('.box').find('.box-body');
 
-            }
-            reader.readAsDataURL(input.files[i]);
-        }
+            wrapperZone.removeClass('dragover');
+            previewZone.removeClass('hidden');
+            boxZone.empty();
+            boxZone.append(htmlPreview);
+        };
+
+        reader.readAsDataURL(input.files[0]);
     }
 }
-
 function reset(e) {
     e.wrap('<form>').closest('form').get(0).reset();
     e.unwrap();
